237fc40b93f14e5efe0c8b525b6d9529141b4adf
banditsbeware <david.rademacher@mavs.uta.edu>
2021-05-16 19:21:22 -0500

    better database interactions

--- app.py
+++ app.py
@@ -13,21 +13,14 @@ def editor():
 
 @app.route('/docs')
 def docs():
-
     conn = db.connect()
-
     posts = db.all_posts(conn)
-
     return render_template('docs.html', posts=posts)
 
 @app.route('/docs/<int:id>')
 def doc(id):
-
     conn = db.connect()
-
     post = db.fetch(conn, id)
-
     return render_template('doc.html', ID=post.id, TITLE=post.title, TEXT=post.body)
-
 
 if __name__ == '__main__': app.run(port=3000)
--- database.py
+++ database.py
@@ -2,52 +2,55 @@ import sqlite3
 import datetime
 
 class doc:
-    def __init__(self, title, id=None, date=None, body=None, tags=None, publish=0, views=0):
+    # default constructor is for creating a new doc, then storing in database
+    def __init__(self, title, date=None, body=None, tags=None, publish=0, views=0):
+        self.id    = None
         self.title = title
-        self.id    = id
         self.date  = date
         self.body  = body
         self.tags  = tags
         self.pub   = publish
         self.views = views
 
-    def set_body(self, text):
-        self.body = text
+    # this constructor can be used when populating an instance from preexisting database entry
+    @classmethod
+    def from_database(cls, conn, id):
+        curs = conn.cursor()
+        curs.execute('SELECT * FROM docs WHERE id=?;', (id,))
+        res = curs.fetchone()
+        if res is None: return None
 
-    def read_body(self, file_name):
-        with open(file_name, 'r') as f: self.body = f.read()
+        id, title, date, body, tags, pub, views = res
+        obj = cls(title, date, body, tags, pub, views)
+        obj.id = id
+        print(obj)
+        return obj
+
+    def set_body(self, text): self.body = text
 
     def save(self, conn):
+    # if self.id is None:
         t = datetime.datetime.now()
-        tup = (t.strftime('%b %d, %Y'), self.title, self.body, self.tags, self.pub, self.views)
-        conn.execute('INSERT INTO docs (date, title, body, tags, pub, views) VALUES (?,?,?,?,?,?);', tup)
+        tup = (self.title, t.strftime('%b %d, %Y'), self.body, self.tags, self.pub, self.views)
+        conn.execute('INSERT INTO docs (title, date, body, tags, pub, views) VALUES (?,?,?,?,?,?);', tup)
         conn.commit()
+    # else:
+        # UPDATE existing record 
 
-    def __repr__(self):
-        return f'[post titled "{self.title}"]'
+    def __repr__(self): return f'[DOC "{self.title}" ({self.views} views)]'
 
-def connect():
-    return sqlite3.connect('database.db')
+def connect(): return sqlite3.connect('database.db')
 
 def all_posts(conn):
     curs = conn.cursor()
-    curs.execute('SELECT * FROM docs WHERE pub=1')
-    posts = []
-    for r in curs.fetchall():
-        posts.append(doc(r[0], r[1], r[2], r[3], r[4], r[5], r[6]))
+    curs.execute('SELECT id FROM docs WHERE pub=1')
+    return [doc.from_database(conn, r[0]) for r in curs.fetchall()]
 
-    return posts
-
-def fetch(conn, id):
-    curs = conn.cursor()
-    curs.execute(f'SELECT * FROM docs WHERE pub=1 AND id={id};')
-    r = curs.fetchone()
-    return doc(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
-
+def fetch(conn, id): return doc.from_database(conn, id)
 
 CREATE_DOCS = '''CREATE TABLE docs (
-    title       TEXT NOT NULL,
     ID          INTEGER PRIMARY KEY AUTOINCREMENT,
+    title       TEXT NOT NULL,
     date        TEXT NOT NULL,
     body        BLOB,
     tags        TEXT,
@@ -63,13 +66,9 @@ if __name__ == '__main__':
     conn.execute(CREATE_DOCS)
 
     for i in range(10):
-        D = doc(title=f'some title {i+1}', publish=1)
-        D.set_body(f'wooo ' * i**2)
+        D = doc(title=f'Title {i+1}', publish=1)
+        D.set_body('wooo yea \n' * (i+1))
         D.save(conn)
-
-    posts = all_posts(conn)
-
-    for p in posts: print(f'{p.id}: {p.title}')
 
     conn.close()
 
