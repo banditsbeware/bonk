6e08883eab8141d9f0b09bca81dfe6c70cf7d27b
banditsbeware <david.rademacher@mavs.uta.edu>
2021-09-05 17:33:54 -0500

    ready to write

--- bonk/routes.py
+++ bonk/routes.py
@@ -1,5 +1,4 @@
 from flask import Flask, Blueprint, render_template, request
-# import database as db
 import os
 
 # router = Flask(__name__, template_folder='scaff')
@@ -8,10 +7,12 @@ router = Blueprint('router', __name__, url_prefix='/')
 
 @router.route('/')
 def index():
+    # get a list of file names
     mats = os.listdir('bonk/scaff/mats')
+    # strip ".html"
+    mats = [ mat[:-5] for mat in mats ]
     return render_template('index.html', mats=mats)
 
 @router.route('/mats/<name>')
 def mat(name):
-    return render_template(f'mats/{name}')
-
+    return render_template(f'mats/{name}.html')
